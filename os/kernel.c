a4143d5527bd78279c2ca87f19ecb9506cc977b6
Li-Ruiqi <1009962019@qq.com>
2024-12-11 17:37:13 +0800

    12.11：实现串口打印控制台

--- os/kernel.c
+++ os/kernel.c
@@ -1,5 +1,12 @@
+extern void uart_init();
+extern void uart_puts(char *s);
+
 void start_kernel(void)
 {
-	while (1) {}; // stop here!
+	uart_init();
+	uart_puts("666\r \n");
+	while (1) {
+		
+	};
 }
 
