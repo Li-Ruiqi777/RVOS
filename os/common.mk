ee446bbc144040c31f8b81b8360cfbf2979aca64
Li-Ruiqi <1009962019@qq.com>
2024-12-12 11:26:45 +0800

    12.12：heap内存管理

--- os/common.mk
+++ os/common.mk
@@ -7,13 +7,14 @@
 # "-mabi=ilp32": 指定编译生成的代码使用 ILP32 数据模型，其中 "I" 表示整数类型使用 32 位，"L" 表示长整数类型使用 32 位，"P" 表示指针类型使用 32 位。
 # "-g": 生成调试信息，以便使用调试器进行调试。
 # "-Wall": 开启所有警告信息，这可以帮助检测代码中的潜在问题。
+# 工具链是运行在64位机器的
 CROSS_COMPILE = riscv64-unknown-elf-
 CFLAGS += -nostdlib -fno-builtin -march=rv32g -mabi=ilp32 -g -Wall
 
 # qemu模拟器相关参数
 # "-nographic": 关闭图形界面，所有输出都在终端中显示
 # "-smp 1": 模拟的Soc只有一个hart
-# "-machine virt": 使用Qemu内置的虚拟平台
+# "-machine virt": 使用Qemu内置的虚拟板子
 # "-bios none": 禁用bios
 QEMU = qemu-system-riscv32
 QFLAGS = -nographic -smp 1 -machine virt -bios none
--- os/kernel.c
+++ os/kernel.c
@@ -1,12 +1,18 @@
-extern void uart_init();
-extern void uart_puts(char *s);
+#include "os.h"
+
+// 这些函数只应该在这里被调用一次，所以只声明一次，不包含在文件 os.h 中
+extern void uart_init(void);
+extern void page_init(void);
+extern void page_test(void);
 
 void start_kernel(void)
 {
 	uart_init();
-	uart_puts("666\r \n");
-	while (1) {
-		
+	uart_puts("Hello RVOS! \r \n");
+
+	page_init();
+	page_test();
+	while (1)
+	{
 	};
 }
-
